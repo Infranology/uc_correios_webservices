1a7fb321e64b3ea6faf0fdf4d71de82268595e0e
Claudio Sanches <claudio.sanches@infranology.com.br>
2012-06-27 16:47:16 -0300

    fixed more code standards

--- tests/CorreiosTest.php
+++ tests/CorreiosTest.php
@@ -1,49 +1,32 @@
 <?php
 
-include_once 'api/correioswebservice.php';
+include_once '../api/correioswebservice.php';
 
-class CorreiosTest extends PHPUnit_Framework_TestCase
-{
+class CorreiosTest extends PHPUnit_Framework_TestCase {
 
-    public static function correiosdata()
-    {
-        return array(
+  public static function correiosdata() {
+    return array(
         //  array($cod_empresa, $senha, $cep_origem, $cep_destino, $altura, $largura, $diametro, $comprimento, $peso, $servico, $valor_declarado, $retorno)
-            array(NULL, NULL, '71939360', '72151613', '5', '15', '0', '20', '2', '41106', '200', 'object'),
-            array(NULL, NULL, '71939360', '72151613', '5', '15', '0', '20', '1', '40010', '200', 'object'),
-            array(NULL, NULL, '71939360', '72151613', '5', '15', '0', '20', '1', '40215', '200', 'object'),
-        );
-    }
- 
-    /**
-     * @dataProvider correiosdata
-     */
-    public function testConnection($cod_empresa,$senha,$cep_origem,$cep_destino,$altura,$largura,$diametro,$comprimento,$peso,$servico,$valor_declarado,$retorno)
-    {
-        $a=calculo_frete_correios_api($cod_empresa, 
-                        $senha, 
-                        $cep_origem, 
-                        $cep_destino, 
-                        $altura,
-                        $largura,
-                        $diametro,
-                        $comprimento,
-                        $peso,
-                        $servico,
-                        $valor_declarado, 
-                        $retorno);
-        print_r($a);
-    }
+        array(NULL, NULL, '71939360', '72151613', '5', '15', '0', '20', '2', '41106', '200', 'object'),
+        array(NULL, NULL, '71939360', '72151613', '5', '15', '0', '20', '1', '40010', '200', 'object'),
+        array(NULL, NULL, '71939360', '72151613', '5', '15', '0', '20', '1', '40215', '200', 'object'),
+    );
+  }
 
-    public function testParseResult()
-    {
+  /**
+   * @dataProvider correiosdata
+   */
+  public function testConnection($cod_empresa, $senha, $cep_origem, $cep_destino, $altura, $largura, $diametro, $comprimento, $peso, $servico, $valor_declarado, $retorno) {
+    $a = calculo_frete_correios_api($cod_empresa, $senha, $cep_origem, $cep_destino, $altura, $largura, $diametro, $comprimento, $peso, $servico, $valor_declarado, $retorno);
+    print_r($a);
+  }
 
+  public function testParseResult() {
+    
+  }
 
-    }
+  public function testReturnError() {
+    
+  }
 
-    public function testReturnError()
-    {
-
-
-    }
 }
