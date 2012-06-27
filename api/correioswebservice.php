283508accd92ea935af892e60174823a288b1318
Claudio Sanches <claudio.sanches@infranology.com.br>
2012-06-27 16:40:51 -0300

    fix code standard

--- api/correioswebservice.php
+++ api/correioswebservice.php
@@ -1,102 +1,87 @@
 <?php
 
-/*
+/**
  *
  * Essa função utiliza o cep do remetente fixo dentro da função
  * Você especifica o cep destino e peso
  * o terceiro parametro é como você quer o retorno:
  * 'objeto', 'array', 'json'
  *
- * Se você precisar especificar mais variáveis para o PAC fique a vontade para atualizar a função
+ * Se você precisar especificar mais variáveis para 
+ * o PAC fique a vontade para atualizar a função
  * XD divirta-se
  *
  */
-function calculo_frete_correios_api($cod_empresa, 
-                        $senha, 
-                        $cep_origem, 
-                        $cep_destino, 
-                        $altura,
-                        $largura,
-                        $diametro,
-                        $comprimento,
-                        $peso = '0.300',
-                        $servico,
-                        $valor_declarado = '0', 
-                        $retorno = 'array')
-{
-   // TRATA OS CEP'S
-   $cep_destino = preg_replace("([^0-9])",'',$cep_destino);
-   $cep_origem = preg_replace("([^0-9])",'',$cep_origem);
- 
-   /*
-    * TIPOS DE FRETE
-    *
-         41106 = PAC sem contrato
-         40010 = SEDEX sem contrato
-         40215 = SEDEX 10, sem contrato
-         40290 = SEDEX Hoje, sem contrato
-         40096 = SEDEX com contrato
-         40436 = SEDEX com contrato
-         40444 = SEDEX com contrato
-         81019 = e-SEDEX, com contrato
-         41068 = PAC com contrato
-    *
-    *
-    */
- 
-   //$webservice = 'http://shopping.correios.com.br/wbm/shopping/script/CalcPrecoPrazo.asmx?WSDL';// URL ANTIGA
-   $webservice = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?WSDL';
- 
-   // TORNA EM OBJETO AS VARIAVEIS
-   $parms = new stdClass;
-   $parms->nCdServico = $servico; //'41106,40010,40215,40290';// PAC, SEDEX E ESEDEX (TODOS COM CONTRATO) - se vc precisar de mais tipos adicione aqui
-   $parms->nCdEmpresa = $cod_empresa;// <- LOGIN DO CADASTRO NO CORREIOS (OPCIONAL)
-   $parms->sDsSenha = $senha;// <- SENHA DO CADASTRO NO CORREIOS (OPCIONAL)
-   $parms->StrRetorno = 'xml';
- 
-   // DADOS DINAMICOS
-   $parms->sCepDestino = $cep_destino;// CEP CLIENTE
-   $parms->sCepOrigem = $cep_origem;// CEP DA LOJA (BD)
-   $parms->nVlPeso = $peso;
- 
-   // VALORES MINIMOS DO PAC (SE VC PRECISAR ESPECIFICAR OUTROS FAÇA ISSO AQUI)
-   $parms->nVlComprimento = $comprimento;
-   $parms->nVlDiametro = $diametro;
-   $parms->nVlAltura = $altura;
-   $parms->nVlLargura = $largura;
- 
-   // OUTROS OBRIGATORIOS (MESMO VAZIO)
-   $parms->nCdFormato = 1;
-   $parms->sCdMaoPropria = 'N';
-   $parms->nVlValorDeclarado = $valor_declarado;
-   $parms->sCdAvisoRecebimento = 'N';
+function calculo_frete_correios_api($cod_empresa, $senha, $cep_origem, $cep_destino, $altura, $largura, $diametro, $comprimento, $peso = '0.300', $servico, $valor_declarado = '0', $retorno = 'array') {
+  // TRATA OS CEP'S
+  $cep_destino = preg_replace("([^0-9])", '', $cep_destino);
+  $cep_origem = preg_replace("([^0-9])", '', $cep_origem);
 
-   // Inicializa o cliente SOAP
-   $soap = @new SoapClient($webservice, array(
-           'trace' => true,
-           'exceptions' => true,
-           'compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP,
-           'connection_timeout' => 1000
-   ));
- 
-   // Resgata o valor calculado
-   $resposta = $soap->CalcPrecoPrazo($parms);
-   $objeto = $resposta->CalcPrecoPrazoResult->Servicos->cServico;
- 
-   // RETORNO
-   if($retorno == 'object')
-   {
-      return $objeto;
-   }
-   elseif($retorno == 'json')
-   {
-      $json = json_encode($objeto);
-      return $json;
-   }
-   else
-   {
-      return $soap;
-   }
+  /**
+   * TIPOS DE FRETE
+   *
+   * 41106 = PAC sem contrato
+   * 40010 = SEDEX sem contrato
+   * 40215 = SEDEX 10, sem contrato
+   * 40290 = SEDEX Hoje, sem contrato
+   * 40096 = SEDEX com contrato
+   * 40436 = SEDEX com contrato
+   * 40444 = SEDEX com contrato
+   * 81019 = e-SEDEX, com contrato
+   * 41068 = PAC com contrato
+   *
+   */
+  $webservice = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?WSDL';
+
+  // TORNA EM OBJETO AS VARIAVEIS
+  $parms = new stdClass();
+  // PAC, SEDEX E ESEDEX (TODOS COM CONTRATO) - se vc precisar de mais tipos adicione aqui
+  $parms->nCdServico = $servico;
+  // LOGIN DO CADASTRO NO CORREIOS (OPCIONAL)
+  $parms->nCdEmpresa = $cod_empresa;
+  // SENHA DO CADASTRO NO CORREIOS (OPCIONAL)
+  $parms->sDsSenha = $senha; 
+  $parms->StrRetorno = 'xml';
+
+  // DADOS DINAMICOS
+  // CEP CLIENTE
+  $parms->sCepDestino = $cep_destino;
+  // CEP DA LOJA (BD)
+  $parms->sCepOrigem = $cep_origem;
+  $parms->nVlPeso = $peso;
+
+  // VALORES MINIMOS DO PAC (SE VC PRECISAR ESPECIFICAR OUTROS FAÇA ISSO AQUI)
+  $parms->nVlComprimento = $comprimento;
+  $parms->nVlDiametro = $diametro;
+  $parms->nVlAltura = $altura;
+  $parms->nVlLargura = $largura;
+
+  // OUTROS OBRIGATORIOS (MESMO VAZIO)
+  $parms->nCdFormato = 1;
+  $parms->sCdMaoPropria = 'N';
+  $parms->nVlValorDeclarado = $valor_declarado;
+  $parms->sCdAvisoRecebimento = 'N';
+
+  // Inicializa o cliente SOAP
+  $soap = @new SoapClient($webservice, array(
+      'trace' => TRUE,
+      'exceptions' => TRUE,
+      'compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP,
+      'connection_timeout' => 1000
+    )
+  );
+
+  // Resgata o valor calculado
+  $resposta = $soap->CalcPrecoPrazo($parms);
+  $objeto = $resposta->CalcPrecoPrazoResult->Servicos->cServico;
+
+  // RETORNO
+  if ($retorno == 'object') {
+    return $objeto;
+  } elseif ($retorno == 'json') {
+    $json = json_encode($objeto);
+    return $json;
+  } else {
+    return $soap;
+  }
 }
-
-?>
